2114e9dd0ace611034858878d4c5853795096d10
Veskfall Games LLC <168687565+Gustavo-Gimenes@users.noreply.github.com>
2024-05-09 14:03:11 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function isMobileDevice() {
     return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
 }
 if (isMobileDevice()) {
-    document.getElementById("thelobangbanner").style.height = "150px";
-    document.getElementById("voidbanner").style.height = "150px";
+    document.getElementById("thelobangbanner").style.height = "170px";
+    document.getElementById("voidbanner").style.height = "170px";
 } else {
-    document.getElementById("thelobangbanner").style.height = "300px";
-    document.getElementById("voidbanner").style.height = "300px";
+    document.getElementById("thelobangbanner").style.height = "400px";
+    document.getElementById("voidbanner").style.height = "400px";
 }
